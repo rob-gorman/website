f6d469a57493b06077c1da1bd9011bfe98f4efb7
Rob <gorman.rob.j@gmail.com>
2022-09-08 19:15:32 -0400

    with node server

--- webserver/main.go
+++ webserver/main.go
@@ -16,12 +16,21 @@ var content embed.FS
 
 func main() {
 	PORT := 3000
+	// var dir string
+
+	// flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	// flag.Parse()
+	dir := http.FileServer(http.Dir("./static"))
 	s := mux.NewRouter()
 
 	static, _ := fs.Sub(content, "static")
 	staticContent := http.FS(static)
 
-	s.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(staticContent))).Methods("GET")
+	s.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", dir))
+	s.PathPrefix("/images/").Handler(http.StripPrefix("/images/", dir))
+
+	// s.Handle("/static/", http.FileServer(staticContent)).Methods("GET")
+	// s.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static")))).Methods("GET")
 	// s.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	// 	http.ServeFile(w, r, "./static/index.html")
 	// }).Methods("GET")
