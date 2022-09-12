a27be45223daf95d80a285e3f8ac5569fbc0f3c5
Rob <gorman.rob.j@gmail.com>
2022-09-12 13:11:19 -0400

    pre-refactor

--- nodeserver/build.sh
+++ nodeserver/build.sh
@@ -1,4 +1,4 @@
 ver=$1
 docker build -t ghcr.io/rob-gorman/nodeserver:${ver} .
 docker push ghcr.io/rob-gorman/nodeserver:${ver}
-# docker run -p3000:3000 --name nodeserver nodeserver:${ver}
+docker run -p3000:3000 --name nodeserver nodeserver:${ver}
